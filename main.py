52661260943b65f6b0b1028d7f8a97e1ca79c48a
ZYY11282021 <ZYY11282021@163.com>
2022-10-24 19:42:04 +0800

    新增加密功能

--- main.py
+++ main.py
@@ -1,25 +1,28 @@
 # -*- coding: UTF-8 -*-
 
 import mods.randomNumber
+import mods.encry
 
 print('\033[32m欢迎使用Z Tool 0.0.1-alpha!\n如果需要帮助，请输入 help')
 
 
 def opt(command):
-    if command == 'help':
-        print('命令列表\nquit 退出：退出Z Tool\n1 随机数：生成给定范围内的随机数\n')
-    elif command == 'quit':
+    if command[0] == 'help':
+        print('命令列表\nquit 退出：退出Z Tool\n1 随机数：生成给定范围内的随机数\n2 加密：加密字符串\n')
+    elif command[0] == 'quit':
         return 'q'
-    elif command == '1':
+    elif command[0] == '1':
         begin = int(input('请输入起始值：'))
         end = int(input('请输入结束值：'))
-        print(mods.randomNumber.generate(begin, end))
+        print('生成的随机数是:', mods.randomNumber.generate(begin, end))
+    elif command[0] == '2':
+        mods.encry.encrypt()
     else:
         print('输入的命令无效，请重新输入')
 
 
 if __name__ == '__main__':
     while True:
-        com = input('请输入：')
+        com = input('请输入：').split()
         if opt(com) == 'q':
             break
